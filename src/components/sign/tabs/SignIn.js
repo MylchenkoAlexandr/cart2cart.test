c09f0d69b71d28b1dfd2a1b9999beec4f11eb0c0
Александр Мыльченко <45290075+MylchenkoAlexandr@users.noreply.github.com>
2021-09-25 21:49:35 +0300

    SignIn

--- src/components/sign/tabs/SignIn.js
+++ src/components/sign/tabs/SignIn.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Input, Checkbox } from 'antd';
+import Button from '../../button';
 
 export default function SignIn() {
     return (
         <div className="SignIn">
-            SignIn
+            <div className="wrapper">
+                <div className="fields">
+                    <div className="field username">
+                        <p>Email</p>
+                        <Input size="large" placeholder="Email" />
+                    </div>
+                    <div className="field password">
+                        <p>Password</p>
+                        <Input.Password  size="large" placeholder="Password" />
+                    </div>
+                    <Checkbox>Stay signed in</Checkbox>
+                    <br/>
+                    <Button title="Sign in"/>
+                </div>
+                <div className="buttons">
+
+                </div>
+            </div>
         </div>
     );
 }
